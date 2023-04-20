b901a4b1d7e77215e9baeceb782b50479c040dd7
tgriffabenitez <tgriffabenitez@gmail.com>
2023-04-20 16:17:18 -0300

    Implemente los metodos de la capa de controller para los genericos

--- ms-actuator/src/main/java/com/sistemasactivos/msactuator/controller/BaseControllerImpl.java
+++ ms-actuator/src/main/java/com/sistemasactivos/msactuator/controller/BaseControllerImpl.java
@@ -4,10 +4,7 @@ import com.sistemasactivos.msactuator.model.Base;
 import com.sistemasactivos.msactuator.service.BaseServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.*;
 
 
 public abstract class BaseControllerImpl <E extends Base, S extends BaseServiceImpl<E, Long>> implements BaseController<E, Long> {
@@ -17,27 +14,32 @@ public abstract class BaseControllerImpl <E extends Base, S extends BaseServiceI
 
     @GetMapping("/")
     public ResponseEntity<?> getAll() {
-        return null;
+        try {
+            return new ResponseEntity<>(service.findAll(), null, 200);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), null, 500);
+        }
    }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getById(Long aLong) {
-        return null;
-    }
+    public ResponseEntity<?> getById(@PathVariable Long id) {
+        try {
+            return new ResponseEntity<>(service.findById(id), null, 200);
 
-    @PostMapping("/")
-    public ResponseEntity<?> save(E entity) {
-        return null;
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<?> update(Long aLong, E entity) {
-        return null;
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), null, 500);
+        }
     }
 
     @DeleteMapping
-    public ResponseEntity<?> delete(Long aLong) {
-        return null;
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        try {
+            return new ResponseEntity<>(service.delete(id), null, 200);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), null, 500);
+        }
     }
 }
 
