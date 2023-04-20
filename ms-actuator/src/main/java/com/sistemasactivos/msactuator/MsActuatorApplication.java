b7cd49066bd12932934e7c48ad42bc086a785f13
tgriffabenitez <tgriffabenitez@gmail.com>
2023-04-20 13:29:23 -0300

    project init

---
+++ ms-actuator/src/main/java/com/sistemasactivos/msactuator/MsActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.sistemasactivos.msactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsActuatorApplication.class, args);
+    }
+
+}
